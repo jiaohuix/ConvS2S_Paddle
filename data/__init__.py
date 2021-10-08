2e250690cd998b9ea047d47fb7df88dc1f4feada
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2021-10-08 11:29:21 +0800

    Update __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -1,2 +1,3 @@
 from .data import prep_dataset,prep_loader,SentsLenDataset,prep_vocab
 from .sampler import DynamicBatchSampler,DynamicTestSampler,DistributedDynamicBatchSampler
+from .sampler_v2 import DistributedDynamicBatchSamplerV2
