3276990656f2540b3e1d62d11c65938d579051cb
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2021-10-08 20:56:19 +0800

    Update bleu.sh

--- bleu.sh
+++ bleu.sh
@@ -2,6 +2,6 @@
 #tar -zxf mosesdecoder.tar.gz
 ## 还原 predict.txt 中的预测结果为 tokenize 后的数据
 sed -r 's/(@@ )|(@@ ?$)//g' output/predict.txt > output/predict.tok.txt
-sed -r 's/(@@ )|(@@ ?$)//g' output/reference.txt > output/reference.tok.de
+sed -r 's/(@@ )|(@@ ?$)//g' output/reference.txt > output/reference.tok.ro
 # 计算multi-bleu
-! perl mosesdecoder/scripts/generic/multi-bleu.perl output/reference.tok.de < output/predict.tok.txt
+! perl mosesdecoder/scripts/generic/multi-bleu.perl output/reference.tok.ro < output/predict.tok.txt
