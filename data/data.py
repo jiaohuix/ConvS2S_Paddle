b8d562016b3405ef1e99a02dc8a150d5823889e6
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2021-10-21 12:23:01 +0800

    Update data.py

--- data/data.py
+++ data/data.py
@@ -180,7 +180,7 @@ def prep_loader(conf, dataset, mode='train', multi_process=False):
                                                          max_sentences=eval(str(max_sentences)),
                                                          bsz_factor=train_args.batch_size_factor, num_replicas=1,
                                                          rank=0, drop_last=False)
-    if conf.model.resume and mode='train':
+    if conf.model.resume and mode=='train':
         batch_sampler.set_epoch(conf.train.last_epoch)
         print(f"----- Resume Training: set sampler's epoch to {conf.train.last_epoch} as a random seed")
 
